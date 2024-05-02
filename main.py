e467276d831c4d16e3ec480238f8d6ce1045dbf8
jamarma <javimartinezmma@gmail.com>
2024-05-02 22:01:00 +0200

    feat: demo bs with opencv

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cv2
-import pybgs as bgs
 
-algorithm = bgs.VuMeter()
+bs = cv2.createBackgroundSubtractorMOG2()
 video_file = "video/Walking.54138969.mp4"
 
 cap = cv2.VideoCapture(video_file)
@@ -9,9 +8,9 @@ cap = cv2.VideoCapture(video_file)
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        output = algorithm.apply(frame)
+        mask = bs.apply(frame)
         cv2.imshow('Original video', frame)
-        cv2.imshow('Foreground Mask', output)
+        cv2.imshow('Foreground Mask', mask)
 
     if cv2.waitKey(1) & 0xFF == 27:  # esc
         print('Exiting...')
