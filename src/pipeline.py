85990452afd06eaeee493adda53ee4a1cef1afd0
Alvaro Berdote <avberdote@hotmail.com>
2024-05-04 22:57:52 +0200

    feat: add new steps and log steps time

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import numpy as np
 from src import step
 
 
@@ -7,15 +7,25 @@ class Pipeline:
 
     def __init__(self, name: str, steps: list[step]):
         self.steps = steps
+        self.times = []
         self.name = name
         print(f'`{self.name}` has been successfully created with {len(self.steps)} steps.')
 
     def run(self, input_params):
         start_time = datetime.now()
         for current_step in self.steps:
-            input_params = current_step.run(*input_params)
+            step_time = datetime.now()
+            input_params = current_step.run(input_params)
+            step_elapsed = (datetime.now() - step_time).total_seconds()
+            self.times.append(step_elapsed)
         end_time = datetime.now()
         elapsed_time = (end_time - start_time).total_seconds()
         print(
             f'Pipeline ´{self.name}´ is completed in {elapsed_time:.2f} seconds')
+        self.times = np.array(self.times)
+        arg_max = np.argmax(self.times)
+        print(
+            f'The slowest step was {self.steps[arg_max].step_name} with {self.times[arg_max]} seconds'
+        )
+        self.times = []
         return input_params
--- src/step.py
+++ src/step.py
@@ -1,4 +1,7 @@
+import warnings
 from abc import ABC, abstractmethod
+import cv2
+import numpy as np
 
 
 class Step(ABC):
@@ -6,5 +9,52 @@ class Step(ABC):
         self.step_name = step_name
 
     @abstractmethod
-    def run(self, inputs):
+    def run(self, inputs, debug: bool):
         pass
+
+    def debug_step(self, image):
+        cv2.imshow(self.step_name, image)
+        cv2.waitKey(0)
+
+
+class BackgroundSubtractionStep(Step):
+
+    def __init__(self, step_name: str, bs_alg):
+        super().__init__(step_name)
+        self.bs_alg = bs_alg
+
+    def run(self, inputs, debug):
+        frame = inputs
+        background_image = self.bs_alg.apply(frame)
+        if debug:
+            self.debug_step(background_image)
+        return background_image
+
+
+class BoundingBoxStep(Step):
+
+    @staticmethod
+    def __filter_bs(bs_image: np.array) -> np.array:
+        _, threshold = cv2.threshold(bs_image, 240, 255, cv2.THRESH_BINARY)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=2)
+        bs_filtered = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+        return bs_filtered
+
+    def __init__(self, step_name: str):
+        super().__init__(step_name)
+
+    def run(self, inputs, debug):
+        bs_raw = inputs
+        bs_filtered = self.__filter_bs(bs_raw)
+        contours, _ = cv2.findContours(bs_filtered, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if contours:
+            all_contour_points = np.concatenate(contours)
+            hull = cv2.convexHull(all_contour_points)
+            if debug:
+                debug_image = bs_raw.copy()
+                cv2.drawContours(debug_image, [hull], -1, (0, 255, 0), 3)
+                self.debug_step(debug_image)
+            return cv2.boundingRect(hull)
+        warnings.warn('No bounding box found', UserWarning)
+        return None
