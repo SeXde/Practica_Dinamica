b96a7eb1e0efb6d8517f5063aaa5e2f505e8ab19
jamarma <javimartinezmma@gmail.com>
2024-05-04 10:11:50 +0200

    feat: hull and bbox of person

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import cv2
+import numpy as np
 
 
-def fill_holes(mask):
-    h, w = mask.shape[:2]
-    for row in range(h):
-        if mask[row, 0] == 255:
-            cv2.floodFill(mask, None, (0, row), 0)
-        if mask[row, w - 1] == 255:
-            cv2.floodFill(mask, None, (w - 1, row), 0)
-    for col in range(w):
-        if mask[0, col] == 255:
-            cv2.floodFill(mask, None, (col, 0), 0)
-        if mask[h - 1, col] == 255:
-            cv2.floodFill(mask, None, (col, h - 1), 0)
-
-    # flood fill background to find inner holes
-    holes = mask.copy()
-    cv2.floodFill(holes, None, (0, 0), 255)
-    holes = cv2.bitwise_not(holes)
-    mask = cv2.bitwise_or(mask, holes)
-    return mask
+def filter_bs(bs_image: np.array) -> np.array:
+    _, threshold = cv2.threshold(bs_image, 240, 255, cv2.THRESH_BINARY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=2)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    return closing
 
 
-bs = cv2.createBackgroundSubtractorMOG2()
+bs = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 video_file = "video/Walking.54138969.mp4"
 
 cap = cv2.VideoCapture(video_file)
@@ -30,11 +18,21 @@ cap = cv2.VideoCapture(video_file)
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        mask = bs.apply(frame)
-        holes = fill_holes(mask.copy())
+        bs_output = bs.apply(frame)
+        mask = filter_bs(bs_output)
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if contours:
+            all_contour_points = np.concatenate(contours)
+            hull = cv2.convexHull(all_contour_points)
+            cv2.drawContours(frame, [hull], -1, (0, 255, 0), 3)
+
+            # Bounding box
+            # x, y, w, h = cv2.boundingRect(hull)
+            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
         cv2.imshow('Original video', frame)
         cv2.imshow('Foreground Mask', mask)
-        cv2.imshow('Foreground Holes', holes)
 
     if cv2.waitKey(1) & 0xFF == 27:  # esc
         print('Exiting...')
